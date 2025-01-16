b4062601bb984b09be3b3a35af18f2d788aab5a1
Cees Bos <dev@ceesbos.nl>
2025-01-16 21:38:44 +0100

    Fix typo

--- challenge-01/src/test/java/dev/cbos/o11yc/challenge01/Challange1ApplicationTests.java
+++ challenge-01/src/test/java/dev/cbos/o11yc/challenge01/Challange1ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Challange1ApplicationTests {
+class Challenge1ApplicationTests {
 
 	@Test
 	void contextLoads() {
